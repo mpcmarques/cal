73e72a90f0f5b4348d838c8d2ff6adaba237b793
Mateus <mpcmarques@icloud.com>
2018-04-07 17:13:27 -0300

    Started drawing on map, need debugging and fix :construction:
    - Todo added with a debugging description in Application.cpp

--- ParkingSpotsSearcher/src/cli/Application.cpp
+++ ParkingSpotsSearcher/src/cli/Application.cpp
@@ -39,33 +39,58 @@ int chooseNearestOrCheapest() {
 }
 
 void showGraphViewer(int opt, int gas) {
-    /* show graph */
-    auto *gv = new GraphViewer(1000, 1000, true);
+    /* show graph
+    auto *gv = new GraphViewer(1000, 1000, false);
     gv->createWindow(1000, 1000);
     gv->defineVertexColor("blue");
     gv->defineEdgeColor("black");
-    gv->defineVertexSize(1);
-    gv->setBackground("maps/map.png");
+    //gv->defineVertexSize(1);
+    gv->setBackground("../maps/images.jpeg");
 
-    /* load osm */
+    /* load osm
     vector<Node> nodes = ApiParser::readNodes("../maps/A.txt");
     vector<Link> links = ApiParser::readNodeLinks("../maps/C.txt");
 
     /* show nodes
     for (Node node: nodes) {
-        gv->addNode((int) node.getId(), (int) node.getLatitude_degrees() * 1000000000,
-                    (int) node.getLongitute_degrees() * 1000000000);
-    }*/
+        gv->addNode((int) node.getId(), (int) node.getLatitude_degrees() * 1000,
+                    (int) node.getLongitute_degrees() * 1000);
+        gv->rearrange();
+    }
 
     /* show roads
     for(Link link: links){
         gv->addEdge((int)link.getId(), (int)link.getNode1_id(), (int)link.getNode2_id(), EdgeType::DIRECTED);
-    }*/
+    }
+
+    gv->rearrange();*/
+
+    /* show graph */
+    auto *gv = new GraphViewer(800, 800, false);
+
+    gv->createWindow(800, 800);
+
+    gv->defineEdgeColor("blue");
+    gv->defineVertexColor("yellow");
+    gv->setBackground("../maps/images.jpeg");
+
+    /* load osm */
+    vector<Node> nodes = ApiParser::readNodes("../maps/A.txt");
+    vector<Link> links = ApiParser::readNodeLinks("../maps/C.txt");
+
+    /* show nodes */
+
+    // TODO: You have to see how much the map changes in latitude and longitude and make a simple rule of three (regra de tres)
+    for (auto node : nodes) {
+        gv->addNode((int)node.getId(), abs((int) node.getLongitute_degrees()) * 100, abs((int) node.getLatitude_degrees())* 100);
+    }
+
+    /* show edges */
+    for (auto link: links){
+        gv->addEdge((int) link.getId(), (int) link.getNode1_id(), (int) link.getNode2_id(), EdgeType::UNDIRECTED);
+    }
 
     gv->rearrange();
-
-
-
 }
 
 int main() {
